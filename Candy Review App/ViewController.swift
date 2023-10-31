09830a1d54a81c3fdb1f9566a28a1110492c05ea
Lorisa Bulosan <lorisab@hawaii.edu>
2023-10-31 13:05:13 -1000

    Initial Commit

---
+++ Candy Review App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Candy Review App
+//
+//  Created by Lorisa Bulosan on 10/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
